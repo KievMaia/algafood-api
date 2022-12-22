7d7f15c44fef17160ca5901f9d7044e20fc11ea4
KievMaia <kievmaia@gmail.com>
2022-12-21 21:08:56 -0300

    Implementando os endpoints de grupos

--- src/main/java/com/algaworks/algafood/domain/repository/GrupoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/GrupoRepository.java
@@ -3,9 +3,9 @@ package com.algaworks.algafood.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.algaworks.algafood.domain.model.Permissao;
+import com.algaworks.algafood.domain.model.Grupo;
 
 @Repository
-public interface PermissaoRepository extends JpaRepository<Permissao, Long>{
+public interface GrupoRepository extends JpaRepository<Grupo, Long>{
 
 }
