8b327ab68eec64efe167d1752d33233d41366174
KievMaia <kievmaia@gmail.com>
2022-12-28 18:09:35 -0300

    Implementando paginação em Cozinhas.

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -9,6 +9,9 @@ import com.algaworks.algafood.domain.model.Cozinha;
 @Repository
 public interface CozinhaRepository extends CustomJpaRepository<Cozinha, Long>{
 
+//	Se quiser paginar a implementação do repository.
+//	Page<Cozinha> findByNomeContaining(String nome, Pageable pageble);
+	
 	List<Cozinha> findByNomeContaining(String nome);
 
 	boolean existsByNome(String nome);
