046f23168ecbbc6a9cfa3c308618cc299c336a53
KievMaia <kievmaia@gmail.com>
2023-01-02 14:22:58 -0300

    Implementação de relatório venda diário com JasperReports

--- src/main/java/com/algaworks/algafood/infrastructure/service/query/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/query/VendaQueryServiceImpl.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.infrastructure.service;
+package com.algaworks.algafood.infrastructure.service.query;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -82,23 +82,46 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 		return listaVendaDiaria;
 	}
 
-	// A construção da consulta está correta, porém não funcionou com banco Postgre.
+	// A construção da consulta está correta, porém não funcionou com banco Postgre. Mas funciona com MySQL.
 	// Algo na conversão para Date na data de criação da entidade Pedido.
 //	@Override
-//	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+//	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
 //		var builder = manager.getCriteriaBuilder();
 //		var query = builder.createQuery(VendaDiaria.class);
 //		var root = query.from(Pedido.class);
+//		var predicates = new ArrayList<Predicate>();
+//		
+//		var functionConvertTzDataCriacao = builder.function(
+//				"convert_tz", Date.class, root.get("dataCriacao"),
+//				builder.literal("+00:00"), builder.literal(timeOffset));
 //		
 //		var functionDateDataCriacao = builder.function(
-//				"date", Date.class, root.get("dataCriacao"));
+//				"date", Date.class, functionConvertTzDataCriacao);
 //		
-//		var selection = builder.construct(VendaDiaria.class, 
+//		var selection = builder.construct(VendaDiaria.class,
 //				functionDateDataCriacao,
 //				builder.count(root.get("id")),
 //				builder.sum(root.get("valorTotal")));
 //		
+//		if (filtro.getRestauranteId() != null) {
+//			predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+//		}
+//	      
+//		if (filtro.getDataCriacaoInicio() != null) {
+//			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), 
+//					filtro.getDataCriacaoInicio()));
+//		}
+//
+//		if (filtro.getDataCriacaoFim() != null) {
+//			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), 
+//					filtro.getDataCriacaoFim()));
+//		}
+//	      
+//		predicates.add(root.get("status").in(
+//				StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+//		
 //		query.select(selection);
+//		query.where(predicates.toArray(new Predicate[0]));
 //		query.groupBy(functionDateDataCriacao);
 //		
 //		return manager.createQuery(query).getResultList();
