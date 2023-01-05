1ad32f9eed4d1dd93b995e2e7aa118b4ba76f025
KievMaia <kievmaia@gmail.com>
2023-01-05 15:22:55 -0300

    Corrigindo exception handler de media type não aceita.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,37 +1,116 @@
 package com.algaworks.algafood.api.controller;
 
-import java.nio.file.Path;
-import java.util.UUID;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.algafood.api.assembler.FotoProdutoModelAssembler;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.model.FotoProdutoModel;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.service.CadastroProdutoService;
+import com.algaworks.algafood.domain.service.CatalogoFotoProdutoService;
+import com.algaworks.algafood.domain.service.FotoStorageService;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteProdutoFotoController {
 
-	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
+	@Autowired
+	private CatalogoFotoProdutoService catalogoFotoProdutoService;
 
-		var nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
+	@Autowired
+	private CadastroProdutoService cadastroProdutoService;
+
+	@Autowired
+	private FotoProdutoModelAssembler fotoProdutoModelAssembler;
+	
+	@Autowired
+	private FotoStorageService fotoStorageService;
+
+	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
+			@Valid FotoProdutoInput fotoProdutoInput) throws IOException {
+		Produto produto = cadastroProdutoService.buscarProdutoPorRestaurante(restauranteId, produtoId);
+
+		MultipartFile arquivo = fotoProdutoInput.getArquivo();
+
+		FotoProduto foto = new FotoProduto();
+		foto.setProduto(produto);
+		foto.setDescricao(fotoProdutoInput.getDescricao());
+		foto.setContentType(arquivo.getContentType());
+		foto.setTamanho(arquivo.getSize());
+		foto.setNomeArquivo(arquivo.getOriginalFilename());
+
+		FotoProduto fotoSalva = catalogoFotoProdutoService.salvar(foto, arquivo.getInputStream());
+
+		return fotoProdutoModelAssembler.toModel(fotoSalva);
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	private FotoProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+		FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+	
+		return fotoProdutoModelAssembler.toModel(fotoProduto);
+	}
+	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+		FotoProduto foto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
 		
-		var arquivoFoto = Path.of("/Users/kiev-/Documents", nomeArquivo);
-		
-		System.out.println(arquivoFoto);
-		System.out.println(fotoProdutoInput.getDescricao());
-		System.out.println(fotoProdutoInput.getArquivo().getContentType());
-		
+		this.catalogoFotoProdutoService.remover(foto);
+	}
+	
+	@GetMapping
+	private ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, 
+			@RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException {
 		try {
-			fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+		
+		FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+		
+		MediaType mediaTypeFoto = MediaType.parseMediaType(fotoProduto.getContentType());
+		List<MediaType> mediaTypesAceitas = MediaType.parseMediaTypes(acceptHeader);
+		
+		verificarCompatibilidadeMediaType(mediaTypeFoto, mediaTypesAceitas);
+		
+		InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+		
+		return ResponseEntity.ok()
+				.contentType(mediaTypeFoto)
+				.body(new InputStreamResource(inputStream));
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
 		}
+	}
+
+	private void verificarCompatibilidadeMediaType(MediaType mediaTypeFoto, List<MediaType> mediaTypesAceitas) throws HttpMediaTypeNotAcceptableException {
+		boolean compativel = mediaTypesAceitas.stream()
+				.anyMatch(mediaTypeAceita -> mediaTypeAceita.isCompatibleWith(mediaTypeFoto));
+		
+		if (!compativel) {
+			throw new HttpMediaTypeNotAcceptableException(mediaTypesAceitas);
+		}
+		
 	}
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.domain.service;
+
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public interface FotoStorageService {
+	
+	InputStream recuperar(String nomeArquivo);
+
+	void armazenar(NovaFoto novaFoto);
+	
+	void remover(String nomeArquivo);
+	
+	default String gerarNomeArquivo(String nomeOriginal) {
+		return UUID.randomUUID().toString() + "_" + nomeOriginal;
+	}
+	
+	@Builder
+	@Getter
+	class NovaFoto {
+		private String nomeArquivo;
+		private InputStream inputStream;
+	}
+
+	default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto) {
+		this.armazenar(novaFoto);
+		
+		if (Objects.nonNull(nomeArquivoAntigo)) {
+			this.remover(nomeArquivoAntigo);
+		}
+	}
+}
