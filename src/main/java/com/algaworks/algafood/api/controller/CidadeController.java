f58724b04d37ed5269deeb655a35558015eab91e
KievMaia <kievmaia@gmail.com>
2023-01-11 15:07:36 -0300

    Implementando globalmente resposta http code para a documentação springfox.

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -26,6 +26,11 @@ import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.repository.CidadeRepository;
 import com.algaworks.algafood.domain.service.CadastroCidadeService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(tags = "Cidades")
 @RestController
 @RequestMapping(value = "/cidades")
 public class CidadeController {
@@ -42,19 +47,26 @@ public class CidadeController {
 	@Autowired
 	private CidadeInputDisassembler cidadeInputDisassembler;
 
+	@ApiOperation("Lista as cidades.")
 	@GetMapping
 	public List<CidadeModel> listar() {
 		return cidadeModelAssembler.toCollectionModel(cidadeRepository.findAll());
 	}
 
+	@ApiOperation("Busca uma cidade por id.")
 	@GetMapping("/{cidadeId}")
-	public CidadeModel buscar(@PathVariable Long cidadeId) {
+	public CidadeModel buscar(
+		@ApiParam(value = "ID de uma cidade", example = "1") 
+		@PathVariable Long cidadeId) {
 		return cidadeModelAssembler.toModel(cadastroCidade.buscarOuFalhar(cidadeId));
 	}
 
+	@ApiOperation("Cadastra uma cidade.")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
+	public CidadeModel adicionar(
+			@ApiParam(name = "corpo", value = "Representação de uma nova cidade") 
+			@RequestBody @Valid CidadeInput cidadeInput) {
 		try {
 			Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
 
@@ -64,8 +76,13 @@ public class CidadeController {
 		}
 	}
 
+	@ApiOperation("Atualiza uma cidade por id.")
 	@PutMapping("/{cidadeId}")
-	public CidadeModel atualizar(@PathVariable Long cidadeId, @RequestBody @Valid CidadeInput cidadeInput) {
+	public CidadeModel atualizar(
+			@ApiParam(value = "Id de uma cidade", example = "1") 
+			@PathVariable Long cidadeId, 
+			@ApiParam(name = "corpo", value = "Representação de uma cidade com os novos dados") 
+			@RequestBody @Valid CidadeInput cidadeInput) {
 
 		try {
 			Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
@@ -81,9 +98,12 @@ public class CidadeController {
 		}
 	}
 
+	@ApiOperation("Exclui uma cidade por id.")
 	@DeleteMapping("/{cidadeId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long cidadeId) {
+	public void remover(
+			@ApiParam(value = "Id de uma cidade", example = "1") 
+			@PathVariable Long cidadeId) {
 		cadastroCidade.excluir(cidadeId);
 	}
 
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,20 +1,70 @@
 package com.algaworks.algafood.core.openapi;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
 
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.OAS_30)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
+				.build()
+			.useDefaultResponseMessages(false)
+			.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+			.apiInfo(apiInfo())
+			.tags(new Tag("Cidades", "Gerencia cidades"));
+	}
+	
+	private List<Response> globalGetResponseMessages(){
+		return Arrays.asList(
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+					.description("Erro interno do servidor")
+					.build(),
+				new ResponseBuilder()
+					.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+					.description("Recurso não possui representação que poderia ser aceito pelo consumidor")
+					.build()
+				
+				//Dessa forma para o springfox 2.X.X
+//				new ResponseMessageBuilder()
+//					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+//					.message("Erro interno do servidor")
+//					.build(),
+//				new ResponseMessageBuilder()
+//					.code(HttpStatus.NOT_ACCEPTABLE.value())
+//					.message("Recurso não possui representação que poderia ser aceito pelo servidor")
+//					.build()
+				);
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Algafood API")
+				.description("API aberta para clientes e restaurantes")
+				.version("1")
+				.contact(new Contact("Kiev Maia", "https://linkedin.com/KievMaia", "kievmaia@gmail.com"))
 				.build();
 	}
 }
