222c377db9fae31631ae620cb5fc148ca4dcf280
KievMaia <kievmaia@gmail.com>
2023-01-26 12:32:22 -0300

    Ajustando o CORS para aplicação de teste javascript.

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +12,18 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeHttpRequests()
+			.authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
+			.cors().and()
 			.oauth2ResourceServer()
 				.opaqueToken();
 	}
+	
+//	@Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+//        return source;
+//    }
 }
--- src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -14,9 +14,9 @@ public class WebConfig implements WebMvcConfigurer{
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
-//			.allowedMethods("*");
-//			.allowedOrigins("*")
+		registry.addMapping("/**")
+			.allowedMethods("*");
+//			.allowedOrigins("*");
 //			.maxAge(30)
 	}
 	
