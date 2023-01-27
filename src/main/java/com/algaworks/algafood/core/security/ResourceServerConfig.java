54602bb29742e59f89bacae5e3e71145febca33d
KievMaia <kievmaia@gmail.com>
2023-01-27 15:24:36 -0300

    Configurando para receber a chave publica. jwt.

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -26,13 +26,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 //				.opaqueToken();
 	}
 	
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKey = new SecretKeySpec("hfd792hf92g732gf379fg3729gf927f32h79".getBytes(), "HmacSHA256");
-		
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
-	}
-	
 //	@Bean
 //    CorsConfigurationSource corsConfigurationSource() {
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
